770dcb0fae3b58d1cb344a57f4757ececc359938
Felipe Firmida <felipefirmida@gmail.com>
2024-08-07 16:23:07 -0300

    Commit inicial
    Formulário em HTML que usa Javascript para comprar Se Número B é maior do que Número A.

---
+++ main.js
@@ -0,0 +1,13 @@
+let button = document.getElementsByTagName('button')[0];
+button.addEventListener('click', function comparaNumeros (event) {
+    event.preventDefault()
+    let numA = document.getElementById('numA');
+    let numB = document.getElementById('numB');
+    
+    if(numB.value > numA.value) {
+        alert(`Formulário válido: O número B (${numB.value}) é maior do que o número A (${numA.value})`)
+    }
+    else {
+        alert(`Formulário inválido: O número B (${numB.value}) NÃO é maior do que o número A (${numA.value})`)
+    }
+})
