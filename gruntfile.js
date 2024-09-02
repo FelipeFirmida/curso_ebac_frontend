9743dac61fd56d8ddc2f05bf3c7334500730df6d
Felipe Firmida <felipefirmida@gmail.com>
2024-09-02 20:03:07 -0300

    Boas Praticas CSS
    Arquivos index.html e main.css editados para ficarem conforme as regras do BEM (Block, Element and Modifier).

---
+++ gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        less: {
+            development: {
+                files: {
+                    'dev/styles/main.css': 'src/styles/main.less'
+                }
+            }
+        }
+    })
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+
+    grunt.registerTask('default', ['less']);
+}
